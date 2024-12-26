18887bccc1eba90dd77cadc5568f6a0d1954e004
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-26 17:36:36 +0700

    26/12/2024

--- handlers/user.php
+++ handlers/user.php
@@ -4,6 +4,14 @@ define('RESET_DATABASE', false);
 require_once __DIR__ . '/../database/sqlite.php';
 
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (preg_match('#^/users/?$#', $requestUri)) {
