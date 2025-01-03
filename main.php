2bcd715af09653170f6706e1193429e8de19af86
Yuda <yuda_p411@apps.ipb.ac.id>
2025-01-03 10:13:39 +0700

    03/01/2025

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 
-define('RESET_DATABASE', true);
+const RESET_DATABASE = true;
 require_once __DIR__ . '/handlers/user.php';
 $port = getenv('PORT') ?: 6003;
 shell_exec('php -S 0.0.0.0:' . $port . ' handlers/user.php');
